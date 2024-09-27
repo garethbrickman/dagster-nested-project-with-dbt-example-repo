11198a24c8827672c324869dfab71cf4c8254a8a
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-09-27 15:08:02 -0400

    add package_data to setup.py

--- dagster/setup.py
+++ dagster/setup.py
@@ -3,6 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="ndr",
     packages=find_packages(),
+    package_data={
+        "ndr": [
+            "dbt-project/**/*",
+        ],
+    },
     install_requires=[
         "dagster",
         "dagster-cloud",
