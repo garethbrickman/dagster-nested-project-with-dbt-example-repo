eb4898bc6b90efc197036bd3a13bf22d87cacc09
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-09-27 15:08:48 -0400

    add packaged_project_dir to project.py

--- dagster/ndr/project.py
+++ dagster/ndr/project.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 ndr_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..", "dbt").resolve()
+    project_dir=Path(__file__).joinpath("..", "..", "..", "dbt").resolve(),
+    packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
 )
 ndr_project.prepare_if_dev()
