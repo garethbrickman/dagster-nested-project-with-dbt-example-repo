a99d9864fbe19bab10607a3d571873aa667af774
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-09-30 15:14:31 -0400

    Revert "add pins back"
    This reverts commit 043f79d44dd3c0cf9420ed9e1779f59cb7250248.

--- dagster/setup.py
+++ dagster/setup.py
@@ -13,10 +13,6 @@ setup(
         "dagster-cloud",
         "dagster-dbt",
         "dbt-duckdb<1.9",
-        "grpcio<1.66.0",
-        "grpcio-health-checking<1.66.0",
-        "protobuf>=4.0,<5.0",
-        "importlib-metadata<7,>=6.0",
     ],
     extras_require={"dev": ["dagster-webserver"]},
 )
