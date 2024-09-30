46f89ac543a2fb8d08fb16206b55edf431eaab41
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-09-30 12:00:29 -0400

    simplify dependencies

--- dagster/setup.py
+++ dagster/setup.py
@@ -12,12 +12,7 @@ setup(
         "dagster",
         "dagster-cloud",
         "dagster-dbt",
-        "pandas",
-        "matplotlib",
         "dbt-duckdb<1.9",
-        "testresources",  # Added to satisfy launchpadlib
-        "importlib-metadata>=6.0,<7",  # Downgraded to satisfy dbt-semantic-interfaces
-        "protobuf>=5.26.1,<6.0dev",  # Upgraded to satisfy grpcio-health-checking
     ],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
+    extras_require={"dev": ["dagster-webserver"]},
 )
