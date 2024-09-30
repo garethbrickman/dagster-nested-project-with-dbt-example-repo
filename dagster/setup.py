9acbd8dd5724c942b9be5984c7e81259a429adf5
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-09-30 11:56:25 -0400

    pin dependencies to fix conflicts during deployment install

--- dagster/setup.py
+++ dagster/setup.py
@@ -15,6 +15,9 @@ setup(
         "pandas",
         "matplotlib",
         "dbt-duckdb<1.9",
+        "testresources",  # Added to satisfy launchpadlib
+        "importlib-metadata>=6.0,<7",  # Downgraded to satisfy dbt-semantic-interfaces
+        "protobuf>=5.26.1,<6.0dev",  # Upgraded to satisfy grpcio-health-checking
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
